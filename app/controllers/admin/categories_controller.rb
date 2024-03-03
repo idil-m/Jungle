9175ead7690ad98197db20fd3d8f085b986c53e7
Idil Mahamed <mahamed.idil@gmail.com>
2024-03-03 02:19:41 -0500

    Displayed a count of how many products are in the database Displayed a count of how many categories are in the database

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,25 @@
+module Admin
+  class CategoriesController < ApplicationController
+    before_action :authenticate
+
+    def index
+      @categories = Category.all
+    end
+
+    def show
+      @category = Category.find(params[:id])
+      @products = @category.products.order(created_at: :desc)
+    end
+
+    private
+
+    def authenticate
+      authenticate_or_request_with_http_basic do |username, password|
+        username == ENV['BASIC_AUTH_USERNAME'] && password == ENV['BASIC_AUTH_PASSWORD']
+      end
+    end
+  end
+end
+
+
+ 
--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,16 @@
 class Admin::DashboardController < ApplicationController
+  before_action :authenticate
+
   def show
+    @products_count = Product.count
+    @categories_count = Category.count
+  end
+
+  private
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['BASIC_AUTH_USERNAME'] && password == ENV['BASIC_AUTH_PASSWORD']
+    end
   end
 end
